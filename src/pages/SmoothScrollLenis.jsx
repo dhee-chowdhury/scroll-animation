b05931b3b39d7539a699cb491a2a2e99f85af68a
dhee-chowdhury <dhee.chowdhury@gmail.com>
2024-11-20 18:23:05 +0600

    added clip path on image

--- src/pages/SmoothScrollLenis.jsx
+++ src/pages/SmoothScrollLenis.jsx
@@ -1,4 +1,9 @@
-import { motion, useScroll } from "framer-motion";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useMotionTemplate,
+} from "framer-motion";
 
 const SmoothScrollLenis = () => {
   return (
@@ -15,7 +20,7 @@ const Hero = () => {
   return (
     <div
       style={{ height: `calc(${SECTION_HEIGHT}px + 100vh)` }}
-      className="w-full relative bg-white"
+      className="w-full relative bg-black"
     >
       <CenterImage />
     </div>
@@ -24,12 +29,28 @@ const Hero = () => {
 export default SmoothScrollLenis;
 
 const CenterImage = () => {
-  const { scrollY, scrollYProgress } = useScroll();
+  const { scrollY } = useScroll();
+  const clip1 = useTransform(scrollY, [0, SECTION_HEIGHT], [25, 0]);
+  const clip2 = useTransform(scrollY, [0, SECTION_HEIGHT], [75, 100]);
+  const clipPath = useMotionTemplate`polygon(${clip1}% ${clip1}%, ${clip2}% ${clip1}%, ${clip2}% ${clip2}%, ${clip1}% ${clip2}%)`;
+
+  const opacity = useTransform(
+    scrollY,
+    [SECTION_HEIGHT, SECTION_HEIGHT + 500],
+    [1, 0]
+  );
+  const backgroundSize = useTransform(
+    scrollY,
+    [0, SECTION_HEIGHT + 500],
+    ["170%", "100%"]
+  );
   return (
     <motion.div
-      animate={{ x: [0, 150] }}
       className="sticky top-0 h-screen w-full"
       style={{
+        opacity,
+        backgroundSize,
+        clipPath,
         backgroundImage:
           "url(https://images.unsplash.com/photo-1460186136353-977e9d6085a1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
         backgroundPosition: "center",
